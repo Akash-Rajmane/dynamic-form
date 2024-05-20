4684399f57d621ec6b4a9f5c886bc584df726ec5
Akash Rajmane <90460418+Akash-Rajmane@users.noreply.github.com>
2024-05-20 15:17:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,67 +3,62 @@ import { v4 as uuid } from "uuid";
 import "./App.css";
 
 function App() {
-  const [formFields, setFormFields] = useState([{ name: "", age: "" }]);
+  const [formFields, setFormFields] = useState([{ id: uuid(), name: "", age: "" }]);
 
-  const handleFormChange = (event, index) => {
-    let data = [...formFields];
-    data[index][event.target.name] = event.target.value;
-    setFormFields(data);
+  const handleFormChange = (event, id) => {
+    const newFormFields = formFields.map(field => {
+      if (field.id === id) {
+        return { ...field, [event.target.name]: event.target.value };
+      }
+      return field;
+    });
+    setFormFields(newFormFields);
   };
 
   const submitHandler = (e) => {
     e.preventDefault();
     console.log(formFields);
-    setFormFields([{ name: "", age: "" }]);
+    setFormFields([{ id: uuid(), name: "", age: "" }]);
   };
 
   const addFields = () => {
-    let newField = {
-      name: "",
-      age: "",
-    };
-
-    setFormFields([...formFields, newField]);
+    setFormFields([...formFields, { id: uuid(), name: "", age: "" }]);
   };
 
-  const removeFields = (index) => {
-    let data = [...formFields];
-    data.splice(index, 1);
-    setFormFields(data);
+  const removeFields = (id) => {
+    const newFormFields = formFields.filter(field => field.id !== id);
+    setFormFields(newFormFields);
   };
 
   return (
     <div className="app">
       <form onSubmit={submitHandler}>
-        {formFields.map((form) => {
-          const id = uuid();
-          return (
-            <div key={id}>
-              <input
-                type="text"
-                name="name"
-                placeholder="Name"
-                onChange={(event) => handleFormChange(event, index)}
-                value={form.name}
-              />
-              <input
-                type="number"
-                name="age"
-                placeholder="Age"
-                onChange={(event) => handleFormChange(event, index)}
-                value={form.age}
-              />
-              <button type="button" onClick={() => removeFields(index)}>
-                Remove
-              </button>
-            </div>
-          );
-        })}
+        {formFields.map((form, index) => (
+          <div key={form.id}>
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              onChange={(event) => handleFormChange(event, form.id)}
+              value={form.name}
+            />
+            <input
+              type="number"
+              name="age"
+              placeholder="Age"
+              onChange={(event) => handleFormChange(event, form.id)}
+              value={form.age}
+            />
+            <button type="button" onClick={() => removeFields(form.id)}>
+              Remove
+            </button>
+          </div>
+        ))}
         <button type="button" onClick={addFields}>
           Add More..
         </button>
         <br />
-        <button type="submit">Submit</button>{" "}
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
